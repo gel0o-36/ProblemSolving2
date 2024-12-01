3797c772a1d4134c352182935436fa872b406a33
ƒailure <123981311+gel0o-36@users.noreply.github.com>
2024-12-01 22:33:38 +0200

    Add files via upload

---
+++ Problem06/main.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+/* ---- Prototype Section ---- */
+int readNumber();
+void reverseNumber(int number);
+/* ---- End Of Prototype Section ---- */
+
+int main() {
+    reverseNumber(readNumber());
+
+    cout << "\n";
+    system("pause");
+    return 0;
+}
+
+int readNumber() {
+    int number;
+    cout << "Enter a number : ";
+    cin >> number;
+    return number;
+}
+
+void reverseNumber(int number) {
+    int newNumber, sum = 0;
+    while (number > 0) {
+        newNumber = number % 10;
+        number = number / 10;
+        cout << newNumber;
+        sum += newNumber;
+    }
+    cout << endl;
+    cout << "Sum : " << sum;
+}
