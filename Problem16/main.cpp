2517345ceed558c7528efe94d08290801ff2ff61
ƒailure <123981311+gel0o-36@users.noreply.github.com>
2024-12-02 16:23:00 +0200

    Add files via upload

---
+++ Problem16/main.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+/* ---- Prototype Section ---- */
+/* ---- End Of Prototype Section ---- */
+
+int main() {
+    cout << "\n";
+    system("pause");
+    return 0;
+}
